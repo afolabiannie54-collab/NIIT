ea9c6084aff53f2ffbe724a09040cc1a918ed10e
afolabiannie54-collab <afolabiannie54@gmail.com>
2026-07-13 09:59:58 +0100

    Delete javaproject/banking-api directory

--- javaproject/banking-api/sql/create-postgres-dbs.sql
+++
@@ -1,8 +0,0 @@
--- Create databases for the banking app (run as a Postgres superuser)
-CREATE DATABASE banking_api_dev;
-CREATE DATABASE banking_api_prod;
-CREATE DATABASE banking_api_qa;
-CREATE DATABASE banking_api_test;
-
--- Example: run from PowerShell (psql must be on PATH):
--- psql -U postgres -W -f sql/create-postgres-dbs.sql
--- javaproject/banking-api/src/main/java/net_banking_api/banking_api/banking/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package net_banking_api.banking_api.banking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net_banking_api.banking_api.banking.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
